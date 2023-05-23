7fb1bb0cd604bb6f8c59bbddbaff81b3ea54aaf9
DaniSuarezG <DanielSuarezGalvez@Gmail.com>
2023-05-23 14:52:38 +0100

    Has Begun

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
-    
+      <div id='container'>
+        <Card/>
+      </div>
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+
+function Card() {
+  return (
+    <div className="card">
+      <img src="/src/assets/image-qr-code.png" alt="" />
+    </div>
+  )
+}
+
+export default Card
