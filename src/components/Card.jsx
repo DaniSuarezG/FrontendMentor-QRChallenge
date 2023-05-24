96dd26e6bf9dafa955b75c4e8c17accb234d3a37
DaniSuarezG <DanielSuarezGalvez@Gmail.com>
2023-05-24 11:09:25 +0100

    Done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,19 @@
+import './Card.css'
 
 function Card() {
   return (
     <div className="card">
       <img src="/src/assets/image-qr-code.png" alt="" />
+      <div className='title'>
+        <p>
+          Improve your front-end skills by building projects
+        </p>
+      </div>
+      <div className="message">
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </p>
+      </div>
     </div>
   )
 }
